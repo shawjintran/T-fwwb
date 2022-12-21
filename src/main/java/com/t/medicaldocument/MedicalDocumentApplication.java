4b96466a35154256012c619c7b42b4658223bcc8
shawjintran <1120420062@qq.com>
2022-12-21 22:26:47 +0800

    init a new project with contest Fwwb

---
+++ src/main/java/com/t/medicaldocument/MedicalDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.t.medicaldocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t/medicaldocument/MedicalDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t.medicaldocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
