1301444475e772c3a86bd414abd8295025e85721
shawjintran <1120420062@qq.com>
2023-06-29 10:51:17 +0800

    modify the requirement sentence

--- src/main/java/com/t/medicaldocument/controller/ApiController.java
+++ src/main/java/com/t/medicaldocument/controller/ApiController.java
@@ -31,11 +31,8 @@ public class ApiController {
     @GetMapping("/ask/{gptRequest}")
     public Object askAi(@PathVariable String gptRequest) {
 
-
-
-
-
-        String replyStr = chartGPTService.send("请只回复相关医学名词以及医学药品,一一列举,不要有其他词语，词语用逗号隔开，不要序列:"+gptRequest);
+        String replyStr = chartGPTService.send("请只回复相关医学专业名词以及医学药品,一一列举,不要有其他多余的字词,"
+            + "不要写出序列,只用逗号连接,如果没有相关的名词或药物就回答:对不起,无相关推荐词"+gptRequest);
 
 
         return replyStr;
